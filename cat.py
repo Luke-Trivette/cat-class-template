5683f650ae4cbfb41812f63af4749dd9df5a2d0a
Luke-Trivette <157540127+Luke-Trivette@users.noreply.github.com>
2024-02-16 10:03:38 -0600

    cat.py

--- cat.py
+++ cat.py
@@ -1,8 +1,16 @@
 #Revmoe pass and complete the cat class
 class Cat():
-    pass
+    def __init__(self, name="Unknown", age=0):
+        self.name = name
+        self.age = age
 
+    def speak(self):
+        return "Meow"
+stella = Cat(name="Stella", age=7)
+garfield = Cat(name="Garfield", age=50)
+stella_speech = stella.speak()
+garfield_speech = garfield.speak()
+print(f"{stella.name} says: {stella_speech}")
+print(f"{garfield.name} says: {garfield_speech}")
 
-#Create objects here
-#These should NOT be indented inside the class
 
